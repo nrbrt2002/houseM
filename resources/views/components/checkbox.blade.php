b6db98548e4b62c70cb04f9552b1e1c5c87f1911
nrbrt2002 <nrbrt2002@gmail.com>
2023-03-28 02:48:08 +0200

    after creating multUser auth with if condition hope to change it to middlewares

---
+++ resources/views/components/checkbox.blade.php
@@ -0,0 +1,1 @@
+<input type="checkbox" {!! $attributes->merge(['class' => 'rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-green-600 shadow-sm focus:ring-green-500 dark:focus:ring-green-600 dark:focus:ring-offset-gray-800']) !!}>
